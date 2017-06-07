7529027c04f4cc9d3a71245ec185cc187a7e4e79
webdkpthroaway <acidchugger@protonmail.com>
2017-06-07 05:24:58 +0300

    Update 20170607011915_world.sql

--- sql/migrations/20170607011915_world.sql
+++ sql/migrations/20170607011915_world.sql
@@ -1,9 +1,10 @@
 INSERT INTO `migrations` VALUES ('20170607011915'); 
 
--- Gossip text for Kandrostrasz
-INSERT INTO `npc_text` VALUES (250, 'A thousand years ago, when the war of the Shifting Sands ended, three of our kin sacrificed themselves and held the Qiraji inside these halls while Anachronos and Staghelm sealed Ahn\'Qiraj shut.$b$bEach of us vowed to look for Merithra, Arygos and Caelestrasz should the Scarab wall ever fall.$b$bAnd fall it did... and so we came.', '', 0, 1, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0);
-INSERT INTO `npc_gossip` (`npc_guid`, `textid`) VALUES (87529, 250);
+-- Andorgos
+UPDATE `creature_template` SET `gossip_menu_id`=6644 WHERE `entry`=15502;
+INSERT INTO `gossip_menu` VALUES (6644, 7899, 0);
+UPDATE `npc_text` SET `text0_0`='' WHERE `ID`=8702;
 
--- Gossip text for Andorgos
-INSERT INTO `npc_text` VALUES (251, '', 'Step carefully mortal. You tread where mightier beings have met their fate.', 0, 1, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0);
-INSERT INTO `npc_gossip` (`npc_guid`, `textid`) VALUES (87527, 251);
+-- Kandrostrasz
+INSERT INTO `gossip_menu` VALUES (6645, 7900, 0);
+UPDATE `creature_template` SET `gossip_menu_id`=6645 WHERE `entry`=15503;
