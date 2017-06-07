49b95b92fb3e8ccded0ecc742372449455913868
webdkpthroaway <acidchugger@protonmail.com>
2017-06-07 04:20:29 +0300

    Added gossip for AQ40 NPCs.

---
+++ sql/migrations/20170607011915_world.sql
@@ -0,0 +1,9 @@
+INSERT INTO `migrations` VALUES ('20170607011915'); 
+
+-- Gossip text for Kandrostrasz
+INSERT INTO `npc_text` VALUES (250, 'A thousand years ago, when the war of the Shifting Sands ended, three of our kin sacrificed themselves and held the Qiraji inside these halls while Anachronos and Staghelm sealed Ahn\'Qiraj shut.$b$bEach of us vowed to look for Merithra, Arygos and Caelestrasz should the Scarab wall ever fall.$b$bAnd fall it did... and so we came.', '', 0, 1, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO `npc_gossip` (`npc_guid`, `textid`) VALUES (87529, 250);
+
+-- Gossip text for Andorgos
+INSERT INTO `npc_text` VALUES (251, '', 'Step carefully mortal. You tread where mightier beings have met their fate.', 0, 1, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO `npc_gossip` (`npc_guid`, `textid`) VALUES (87527, 251);
